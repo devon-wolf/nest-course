2042a0e3b1ee9342a40097952044290b4e81ae7c
Devon Wolfkiel <devon.wolfkiel@hingehealth.com>
2023-05-27 10:49:14 -0700

    add post route

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
+  @Post()
+  create(@Body() body: unknown) {
+    console.log('[POST]', body);
+    return 'This action creates a coffee';
+  }
+
   @Get()
   findAll() {
     return 'This action returns all coffees.';
