d2c931fb58e575179dbf9ecec70362bde3aa7fea
Devon Wolfkiel <git@devonwolf.simplelogin.com>
2023-08-16 16:27:22 -0700

    start adding openapi details

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,7 +15,9 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { Protocol } from 'src/common/decorators/protocol.decorator';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
@@ -25,6 +27,7 @@ export class CoffeesController {
     return this.coffeesService.create(createCoffeeDto);
   }
 
+  @ApiForbiddenResponse({ description: 'Forbidden' })
   @Public()
   @Get()
   async findAll(
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as path from 'path';
+import * as fs from 'fs';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,11 +21,31 @@ async function bootstrap() {
       },
     }),
   );
+
   app.useGlobalFilters(new HttpExceptionFilter());
+
   app.useGlobalInterceptors(
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('nest-course')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  try {
+    const filepath = path.resolve(process.cwd(), 'open-api.json');
+    fs.writeFileSync(filepath, JSON.stringify(document), 'utf8');
+  } catch (error) {
+    console.error('Unable to generate open-api.json: ', error);
+  }
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
